da705181649966699bb2f439f66916796dc4aaab
AyushSundriyal21 <abhayush7@gmail.com>
2025-07-22 22:48:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
-pos = 0
-snake_body = []
-for i in range(3):
-    tim = Turtle(shape="square")
-    snake_body.append(tim)
-    tim.color("white")
-    tim.speed(0)
-    tim.penup()
-    tim.setpos(x=pos, y=0)
-    pos -= 20
+screen.listen()
+snake = Snake()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
 screen.update()
 
 while True:
     screen.update()
     time.sleep(0.1)
-    for body_num in range(len(snake_body) - 1, 0, -1):
-        new_x = snake_body[body_num - 1].xcor()
-        new_y = snake_body[body_num - 1].ycor()
-        snake_body[body_num].goto(new_x, new_y)
+    snake.move()
 
-    snake_body[0].fd(20)
-    snake_body[0].left(90)
 
 
 
